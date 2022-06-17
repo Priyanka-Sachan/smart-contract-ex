c8711f8fbffd772c678afbe69460e1a26053d3b0
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-06-17 17:05:42 +0530

    Upload NFTs to pinata

--- NFT/deploy/02-deploy-random-ipfs-nft.js
+++ NFT/deploy/02-deploy-random-ipfs-nft.js
@@ -1,19 +1,37 @@
 const { network } = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata.js")
 
 const FUND_AMOUNT = "1000000000000000000000"
+const imagesLocation = "./images/randomNft/"
 let tokenUris = [
   "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
   "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
   "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
 ]
 
+const metadataTemplate = {
+  name: "",
+  description: "",
+  image: "",
+  attributes: [
+    {
+      trait_type: "Cuteness",
+      value: 100,
+    },
+  ],
+}
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
   let vrfCoordinatorV2Address, subscriptionId
+
+  if (process.env.UPLOAD_TO_PINATA == "true") {
+    tokenUris = await handleTokenUris()
+  }
 
   if (chainId == 31337) {
     // create VRFV2 Subscription
@@ -51,6 +69,26 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log("Verifying...")
     await verify(randomIpfsNft.address, arguments)
   }
+}
+
+async function handleTokenUris() {
+  // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+  // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+  // You could also look at pinata https://www.pinata.cloud/
+  tokenUris = []
+  const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+  for (imageUploadResponseIndex in imageUploadResponses) {
+    let tokenUriMetadata = { ...metadataTemplate }
+    tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+    tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+    tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+    console.log(`Uploading ${tokenUriMetadata.name}...`)
+    const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+    tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+  }
+  console.log("Token URIs uploaded! They are:")
+  console.log(tokenUris)
+  return tokenUris
 }
 
 module.exports.tags = ["all", "randomipfs", "main"]
